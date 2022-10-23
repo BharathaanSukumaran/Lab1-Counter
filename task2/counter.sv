14da7f45487687853ef92a3ecb3032572917beed
BharathaanSukumaran <bharathaan@LAPTOP-31N15V8L.localdomain>
2022-10-23 01:57:59 +0100

    Completed

---
+++ task2/counter.sv
@@ -0,0 +1,22 @@
+module counter #(
+    // bits in counter set to 8 bits
+    parameter WIDTH = 8
+)(
+    // interface signals
+    input logic clk, //clk
+    input logic rst, //reset
+    input logic en, // enable
+    output logic [WIDTH-1:0] count //count output
+);
+// indicates that this is a clocked circuit
+always_ff@(posedge clk) 
+// {WIDTH{1’b0}} in line 12 uses the concatenation operator { } to form WIDTH bits of ‘0
+if(rst)begin
+count <= {WIDTH{1'b0}};
+end
+else if(en)
+count <= count + 4'h0001;
+else
+count <= count - 4'h0001;
+endmodule 
+
---
+++ task2/counter_tb.cpp
@@ -0,0 +1,68 @@
+#include "Vcounter.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "vbuddy.cpp"
+
+int main(int argc, char **argv, char **env){
+    int it;
+    int clk;
+
+    Verilated::commandArgs(argc,argv);
+//  initialise top verilog instance
+    Vcounter* top = new Vcounter;
+// initialise trace dump
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace(tfp,99);
+    tfp->open("counter.vcd");
+
+    // init Vbuddy
+    if (vbdOpen()!=1) return (-1);
+    vbdHeader("Fuck This Shit");
+
+
+    // initialise simulation outputs
+    top->clk =1;
+    top->rst=0;
+    top->en = vbdFlag();
+
+    // run simulation for many clock cycles
+    for(int i=0;i<300; i++){
+        
+
+        // dump variables into VCD file and toggle clock
+        for(clk=0;clk<2;clk++){
+            // in ps
+            tfp->dump (2*i+clk);
+            // falling edge
+            top->clk = !top->clk;
+            top->eval ();
+        }
+
+    // Send count value to Vbuddy
+        vbdHex(4, (int(top->count) >> 16) & 0xF);
+        vbdHex(3, (int(top->count) >> 8) & 0xF);
+        vbdHex(2, (int(top->count) >> 4) & 0xF);
+        vbdHex(1, int(top->count) & 0xF);
+        vbdCycle(i+1);
+
+    // Plot count on display
+        // vbdPlot(int(top->count), 0, 255);
+        
+        // Change input stimuli
+        // top->rst =(i<2) | (i==15);
+        top->en = vbdFlag();
+        // if(top-> en == 1){
+        //     top->count = top->count + 1;
+        // }
+        // else{
+        //     top->count = top->count - 1;
+        // }
+        if(Verilated::gotFinish()) exit(0);
+    }
+    vbdClose();
+    tfp->close();
+    exit(0);
+
+}
+
